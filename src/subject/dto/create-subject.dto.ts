450db2295ea9e37eed7847283b9022380616c340
lucianosants <lucianosantos.developer@gmail.com>
2023-11-20 09:26:22 -0300

    feat(subject): implements subject creation route

--- src/subject/dto/create-subject.dto.ts
+++ src/subject/dto/create-subject.dto.ts
@@ -1,1 +1,8 @@
-export class CreateSubjectDto {}
+import { IsString, MinLength, IsNotEmpty } from 'class-validator';
+
+export class CreateSubjectDto {
+  @IsString({ message: 'Informe uma disciplina válida.' })
+  @MinLength(3, { message: 'A disciplina deve ter no mínimo 3 caracteres.' })
+  @IsNotEmpty({ message: 'Uma disciplina deve ser informada.' })
+  name: string;
+}
--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { IsAlreadyError } from 'src/shared/errors/isAlready.error';
 
 @Injectable()
 export class SubjectService {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createSubjectDto: CreateSubjectDto) {
-    return 'This action adds a new subject';
+  async create(createSubjectDto: CreateSubjectDto) {
+    const subjectFound = await this.prisma.subject.findFirst({
+      where: { name: createSubjectDto.name },
+    });
+
+    if (subjectFound) {
+      const errorMessage = `${createSubjectDto.name} já está existe.`;
+      throw new IsAlreadyError(errorMessage).showError();
+    }
+
+    await this.prisma.subject.create({
+      data: {
+        name: createSubjectDto.name,
+      },
+    });
+
+    return { message: `${createSubjectDto.name} cadastrada com sucesso.` };
   }
 }
